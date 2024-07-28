3e335396e01314b41d64e5a26d5efdb622563f07
BUNYODBEK <bunyodbekxalilov@gmail.com>
2024-07-29 02:17:27 +0300

    birnchi tushunchalar

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+komentariya
+"""
+# komentariya
+
+# # print("tug'ilgan yilingizni aniqlang")
+# a = int(input())
+# b = 2024
+# c = b - a
+# print(f"Siz taxminan {c} yilda tug'ilgansiz shunyadmi?")
+
+
+a = 'qwer'
+b = 'qwer'
+c = a == b
+print(c)

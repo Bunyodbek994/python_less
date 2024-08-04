779d904b65b1fff0421930983d17ed8a4fb34009
BUNYODBEK <bunyodbekxalilov@gmail.com>
2024-08-04 18:37:52 +0300

    birinchi tushunchalar

--- les2.py
+++ les2.py
@@ -1,5 +1,25 @@
 # Birinchi tushunchalar
 
+# list_1 = [36]
+# list_2 = list(1, 4, 6, 9)
+list_3 = []
+print(*list_3)
+
+# for i in range(11):
+#     list_3.append(i)    # .append() bu mavjud masivlarga yana bir masiv qoshadi.
+#     print(list_3)
+# print(list_3)
+# for i in range(10):
+#     list_3.pop()  # bu esa mavjud masivlardan birini ochiradi
+#     print(list_3)
+
+list_3 = [1, 2, 3, 4, 5, 6]
+print(list_3.insert(2, 50 ))   # bu (har qaysi masivni suirb) aynan ko'rsatilgan masivga yangi qiymat qoshaddi 
+                               #misol: 2- masivga 50 qiymatini qoshyapti
+
+print(list_3)
+
+
 # Ikkinchi tushunchalar
 
 # Uchini tushunchalar
